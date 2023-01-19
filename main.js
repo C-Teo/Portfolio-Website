3417bc5539ed24845b07470da858b2209f0d27c1
Teo <90730116+C-Teo@users.noreply.github.com>
2023-01-19 12:13:22 -0500

    Dark Mode, Light Mode, Switch, Color Themes

---
+++ main.js
@@ -0,0 +1,28 @@
+var flag = true; // True is light theme, False is dark theme
+
+function switchTheme() {
+    if (flag) {
+        document.body.style.backgroundColor = "rgb(54, 54, 54)";
+        document.body.style.color = "white";
+        document.getElementsByClassName("footer")[0].style.backgroundColor = "rgb(41, 41, 41)";
+        document.getElementsByClassName("navbar")[0].style.backgroundColor = "rgb(41, 41, 41)";
+        var links = document.getElementsByClassName("navlink");
+        for (var i = 0; i < links.length; i++) {
+            links[i].style.color = "white";
+        }
+        document.getElementById("footer-text").style.color = "white";
+        document.getElementById("copyright").style.color = "rgb(101, 162, 237)";
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        document.getElementsByClassName("footer")[0].style.backgroundColor = "rgb(228, 228, 228)";
+        document.getElementsByClassName("navbar")[0].style.backgroundColor = "rgb(228, 228, 228)";
+        var links = document.getElementsByClassName("navlink");
+        for (var i = 0; i < links.length; i++) {
+            links[i].style.color = "black";
+        }
+        document.getElementById("footer-text").style.color = "black";
+        document.getElementById("copyright").style.color = "rgb(32, 96, 174)";
+    }
+    flag = !flag;
+}
